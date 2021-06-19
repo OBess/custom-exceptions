4978e3aed0d1e4205b284c9dac77f29b718d9b6f
OBess <dima.suschenko03@gmail.com>
2021-06-19 11:33:16 +0300

    Added two functions and enum error codes

--- main.cpp
+++ main.cpp
@@ -1,8 +1,39 @@
 // C++
 #include <iostream>
 
+// Custom
+#include "include/custom_exceptions.hpp"
+
+// Enum
+enum Errors
+{
+   FILE_OPEN_FAILED = -5,
+   SIZE_DETERMINATION_FAILED,
+   MEMORY_ALLOCATION_FAILED,
+   READ_FAILED,
+   FILE_CLOSE_FAILED
+};
+
+// Prototypes
+void read_file_try();
+Errors read_file_code();
+
 int main()
 {
+   read_file_try();
+   auto errorCode = read_file_code();
 
    return EXIT_SUCCESS;
 }
+
+// Implementations
+void read_file_try()
+{
+
+}
+
+Errors read_file_code()
+{
+   return Errors::FILE_CLOSE_FAILED;
+}
+
