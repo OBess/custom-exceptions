d1882c2fd8401d671d5292e744633dd4c1fc0578
OBess <dima.suschenko03@gmail.com>
2021-06-19 11:25:50 +0300

    Defined all necessary classes that based on std::exception

--- include/custom_exceptions.hpp
+++ include/custom_exceptions.hpp
@@ -3,3 +3,42 @@
 // C++
 #include <exception>
 
+struct fileOpenFailed : public std::exception
+{
+   const char *what() const override
+   {
+      return "File is exist or filename is not right.";
+   }
+};
+
+struct sizeDeterminationFailed : public std::exception
+{
+   const char *what() const override
+   {
+      return "Size of file is small.";
+   }
+};
+
+struct memoryAllocationFailed : public std::exception
+{
+   const char *what() const override
+   {
+      return "Memory can not be allocated.";
+   }
+};
+
+struct readFailed : public std::exception
+{
+   const char *what() const override
+   {
+      return "Can not read file.";
+   }
+};
+
+struct fileClosedFailed : public std::exception
+{
+   const char *what() const override
+   {
+      return "Can not close file.";
+   }
+};
