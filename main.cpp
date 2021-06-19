f796b189d3cb152cf449e69ba441066b8962e741
OBess <dima.suschenko03@gmail.com>
2021-06-19 12:18:49 +0300

    Finished read_file_code

--- main.cpp
+++ main.cpp
@@ -8,32 +8,36 @@
 // Enum
 enum Errors
 {
+   SUCCESSFUL = 0,
    FILE_OPEN_FAILED = -5,
-   length_DETERMINATION_FAILED,
+   SIZE_DETERMINATION_FAILED,
    MEMORY_ALLOCATION_FAILED,
    READ_FAILED,
    FILE_CLOSE_FAILED
 };
 
 // Prototypes
-void read_file_try();
-Errors read_file_code();
+void read_file_try(const std::string path);
+Errors read_file_code(const std::string path);
 
 int main()
 {
-   read_file_try();
-   Errors errorCode = read_file_code();
+   std::string filename = "test.txt";
+
+   read_file_try(filename);
+
+   Errors errorCode = read_file_code(filename);
    std::cout << errorCode << std::endl;
+
    return EXIT_SUCCESS;
 }
 
 // Implementations
-void read_file_try()
+void read_file_try(const std::string path)
 {
-   std::string filename = "test.txt";
    try
    {
-      std::ifstream file(filename);
+      std::ifstream file(path);
 
       if (!file.is_open())
          throw fileOpenFailed();
@@ -83,7 +87,39 @@ void read_file_try()
    }
 }
 
-Errors read_file_code()
+Errors read_file_code(const std::string path)
 {
-   return Errors::FILE_CLOSE_FAILED;
+   Errors code = Errors::SUCCESSFUL;
+
+   std::ifstream file(path);
+
+   if (file.is_open())
+   {
+      file.seekg(0, file.end);
+      auto length = file.tellg();
+      // For test 'readFailed', comment line under this
+      file.seekg(0, file.beg);
+
+      if (length > 0)
+      {
+         char *buff = new char[length];
+         if (buff)
+            if (file.read(buff, length))
+            {
+               file.close();
+               if (file.fail())
+                  code = Errors::FILE_CLOSE_FAILED;
+            }
+            else
+               code = Errors::READ_FAILED;
+         else
+            code = Errors::MEMORY_ALLOCATION_FAILED;
+      }
+      else
+         code = Errors::SIZE_DETERMINATION_FAILED;
+   }
+   else
+      code = Errors::FILE_OPEN_FAILED;
+
+   return code;
 }
